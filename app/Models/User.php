688eb8df908caf263892e45999de97210f47f747
Hajer <hajer.benlassoued@gmail.com>
2026-04-05 00:09:06 +0200

    Fix working with scss

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Database\Factories\UserFactory;                         // Factories are used in testing or seeding to quickly generate fake users
+use Illuminate\Database\Eloquent\Attributes\Fillable;       // These are attribute helpers
+use Illuminate\Database\Eloquent\Attributes\Hidden;         
+use Illuminate\Database\Eloquent\Factories\HasFactory;      // HasFactory is a traitb : Allows you to use model factories to create test data
+use Illuminate\Foundation\Auth\User as Authenticatable;     // This gives your model login, password, and authentication features
+use Illuminate\Notifications\Notifiable;                    // Notifiable is a trait : Lets your User receive notifications, like emails, SMS, or Slack messages
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Default Title')</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    {{-- Start Navigation Bar   --}}
+    @include('layout.navbar')
+    {{-- End Navigation Bar     --}}
+
+
+    {{-- Start Sidebar section   --}}
+     @include('layout.sidebar')
+     {{-- End Sidebar section   --}}
+
+    {{-- Start Page Content --}}
+    @yield('content')
+    {{-- End Page Content --}}
+
+</body>
+</html>
